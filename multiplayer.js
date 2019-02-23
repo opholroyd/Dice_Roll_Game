e2d48fc8d49e5627e80e10e04ed85b382ac69ed6
Oliver Patrick <46892114+opholroyd@users.noreply.github.com>
2019-02-23 22:57:28 +0000

    Add files via upload

--- multiplayer.js
+++ multiplayer.js
@@ -1,66 +1,35 @@
-const rollP1 = document.getElementById("btnP1");
-const rollP2 = document.getElementById("btnP2");
-const holdP1 = document.getElementById("holdbtn1");
-const holdP1 = document.getElementById("holdbtn2");
+let scores, roundScore, activePlayer,gamePlaying;
+const rollbtn = document.getElementById("btn");
+const holdbtn = document.getElementById("holdbtn");
 
-let scoreP1 = 0;
-let scoreP2 = 0;
-let playerOne = false
-let playerTwo = false
+rollbtn = document.getElementById("btn").addEventListener('click', function(){
+	if (gamePlaying) {
+		// 1. random number
+		var dice = Math.floor(Math.random() * 6) + 1;
 
-rollP1.addEventListener("click", () => {
-    playerOne = true
-    roll();
-})
+		// 2. display result
+		var diceDOM = document.querySelector('.dice');
+		diceDOM.style.display = 'block';
+		diceDOM.src = diceimgs['diceimg' + activePlayer + dice];
 
+		document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em';
 
-rollP2.addEventListener("click", () => {
-    playerTwo = true
-    roll();
-})
+		// 3. Update round score if the rolled number is not 1
+		if (dice !== 1) {
+			hideRolledMsg();
+			//add score
+			roundScore += dice;
+			document.querySelector('#current-' + activePlayer).textContent = roundScore;
+		} else {
+			//disable button
 
-const roll = () => {
-       let num = Math.floor(Math.random()*6);
-       if (playerOne = true || playerTwo = true) {
-        if (num == 1) {
-            gameOver()
-            console.log("1")
-        } if (num == 2) {
-            document.getElementById("demo1").innerHTML = score += num;
-            document.getElementById("dice").src="./img/dice2.png";
-            console.log("2")
-        } if (num == 3) {
-            document.getElementById("demo1").innerHTML = score += num;
-            document.getElementById("dice").src="./img/dice3.png";
-            console.log("3")
-        } if (num == 4) {
-            document.getElementById("demo1").innerHTML = score += num;
-            document.getElementById("dice").src="./img/dice4.png";
-            console.log("4")
-        } if (num == 5) {
-            document.getElementById("demo1").innerHTML = score += num;
-            document.getElementById("dice").src="./img/dice5.png";
-            console.log("5")
-        } if (num == 6) {
-            document.getElementById("demo1").innerHTML = score += num;
-            document.getElementById("dice").src="./img/dice6.png";
-            console.log("6")
-        } if (score >= 20) {
-            winner();
-            start = false;
-            score = 0;
-        }
-    }
-};
+			disableBtn(btnRoll, 1000);
+			hideRolledMsg();
+			document.querySelector('.player-'+activePlayer+'-rolled-1').style.visibility = 'visible';
+			nextPlayer();		
+		}
+	}
+	
+		
+});
 
-const reset = () => {
-    document.getElementById("demo1").innerHTML = "You rolled a one. You lose.";
-    document.getElementById("demo1").style.color = "red";
-    document.getElementById("dice").src="./img/dice1.png";
-    score = 0;
-}
-
-const winner = () => {
-    // document.getElementById("demo1").innerHTML = "You Win!";
-    // document.getElementById("demo1").style.color = "green";
-}
